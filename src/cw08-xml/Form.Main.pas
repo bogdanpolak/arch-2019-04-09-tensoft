fb00192e808f71bc91b690cbce07a8b09e87479d
Bogdan Polak <bogdan.polak@bsc.com.pl>
2019-04-08 19:52:33 +0200

    Poprawiono formatowanie sekcji uses

--- src/cw08-xml/Form.Main.pas
+++ src/cw08-xml/Form.Main.pas
@@ -4,9 +4,11 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants,
-  System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.StdCtrls, Xml.xmldom,
-  Xml.XMLIntf, Xml.XMLDoc, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  System.Classes,
+  Vcl.Graphics, Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls,
+  Vcl.StdCtrls,
+  Xml.xmldom, Xml.XMLIntf, Xml.XMLDoc,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option,
   FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
   FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.SQLite,
   FireDAC.Phys.SQLiteDef, FireDAC.Stan.ExprFuncs, FireDAC.VCLUI.Wait,
@@ -46,11 +48,9 @@ begin
   FDQuery1.WhileNotEof(
     procedure()
     begin
-      Memo1.Lines.Add(Format('%d-%.2d : %d',[
-        FDQuery1.FieldByName('Year').AsInteger,
-        FDQuery1.FieldByName('Month').AsInteger,
-        FDQuery1.FieldByName('OrderCount').AsInteger
-      ]))
+      Memo1.Lines.Add(Format('%d-%.2d : %d',
+        [FDQuery1.FieldByName('Year').AsInteger, FDQuery1.FieldByName('Month')
+        .AsInteger, FDQuery1.FieldByName('OrderCount').AsInteger]))
     end);
 end;
 
